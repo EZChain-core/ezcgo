2a1ea51fe8ffb0a8d13b7d85455db259b54def81
enixdark <cqshinn92@gmail.com>
2022-01-10 03:31:31 +0700

    feat (update): implement unittest for reward

--- vms/platformvm/reward_test.go
+++ vms/platformvm/reward_test.go
@@ -4,118 +4,106 @@
 package platformvm
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
 	"github.com/ava-labs/avalanchego/utils/units"
 )
 
+func isPowerTwo(num int) bool {
+	return (num != 0) && ((num & (num - 1)) == 0);
+}
+
 func TestRewardLongerDurationBonus(t *testing.T) {
 	shortDuration := 24 * time.Hour
 	totalDuration := 365 * 24 * time.Hour
-	shortBalance := units.KiloAvax
+	shortBalance := units.MegaAvax
+	REWARD_POOL := 500 * units.MegaAvax
+	var TotalStake uint64 = 0
 	for i := 0; i < int(totalDuration/shortDuration); i++ {
-		r := reward(shortDuration, shortBalance, 359*units.MegaAvax+shortBalance, defaultMaxStakingDuration)
+		r := rewardEZC(
+			uint(shortDuration.Seconds()),
+			fromEZC(float64(shortBalance)),
+			fromEZC(float64(TotalStake)),
+			fromEZC(float64(REWARD_POOL)),
+			)
 		shortBalance += r
+		TotalStake += shortBalance
+		REWARD_POOL = REWARD_POOL - r
 	}
-	r := reward(totalDuration%shortDuration, shortBalance, 359*units.MegaAvax+shortBalance, defaultMaxStakingDuration)
+	r := rewardEZC(
+		uint((totalDuration%shortDuration).Seconds()),
+		fromEZC(float64(shortBalance)),
+		fromEZC(float64(TotalStake)), fromEZC(float64(REWARD_POOL)),
+		)
 	shortBalance += r
+	TotalStake += shortBalance
+	REWARD_POOL = REWARD_POOL - r
 
-	longBalance := units.KiloAvax
-	longBalance += reward(totalDuration, longBalance, 359*units.MegaAvax+longBalance, defaultMaxStakingDuration)
-
+	// reset reward pool for compare
+	REWARD_POOL = 500 * units.MegaAvax
+	longBalance := units.MegaAvax
+	longBalance += rewardEZC(
+		uint(totalDuration.Seconds()),
+		fromEZC(float64(longBalance)),
+		fromEZC(float64(0)),
+		fromEZC(float64(REWARD_POOL)),
+		)
 	if shortBalance >= longBalance {
 		t.Fatalf("should promote stakers to stake longer")
 	}
 }
 
-func TestRewards(t *testing.T) {
-	tests := []struct {
-		duration       time.Duration
-		stakeAmount    uint64
-		existingAmount uint64
-		expectedReward uint64
-	}{
-		// Max duration:
-		{ // (720M - 360M) * (1M / 360M) * 12%
-			duration:       defaultMaxStakingDuration,
-			stakeAmount:    units.MegaAvax,
-			existingAmount: 360 * units.MegaAvax,
-			expectedReward: 120 * units.KiloAvax,
-		},
-		{ // (720M - 400M) * (1M / 400M) * 12%
-			duration:       defaultMaxStakingDuration,
-			stakeAmount:    units.MegaAvax,
-			existingAmount: 400 * units.MegaAvax,
-			expectedReward: 96 * units.KiloAvax,
-		},
-		{ // (720M - 400M) * (2M / 400M) * 12%
-			duration:       defaultMaxStakingDuration,
-			stakeAmount:    2 * units.MegaAvax,
-			existingAmount: 400 * units.MegaAvax,
-			expectedReward: 192 * units.KiloAvax,
-		},
-		{ // (720M - 720M) * (1M / 720M) * 12%
-			duration:       defaultMaxStakingDuration,
-			stakeAmount:    units.MegaAvax,
-			existingAmount: SupplyCap,
-			expectedReward: 0,
-		},
-		// Min duration:
-		// (720M - 360M) * (1M / 360M) * (10% + 2% * MinimumStakingDuration / MaximumStakingDuration) * MinimumStakingDuration / MaximumStakingDuration
-		{
-			duration:       defaultMinStakingDuration,
-			stakeAmount:    units.MegaAvax,
-			existingAmount: 360 * units.MegaAvax,
-			expectedReward: 274122724713,
-		},
-		// (720M - 360M) * (.005 / 360M) * (10% + 2% * MinimumStakingDuration / MaximumStakingDuration) * MinimumStakingDuration / MaximumStakingDuration
-		{
-			duration:       defaultMinStakingDuration,
-			stakeAmount:    defaultMinValidatorStake,
-			existingAmount: 360 * units.MegaAvax,
-			expectedReward: 1370,
-		},
-		// (720M - 400M) * (1M / 400M) * (10% + 2% * MinimumStakingDuration / MaximumStakingDuration) * MinimumStakingDuration / MaximumStakingDuration
-		{
-			duration:       defaultMinStakingDuration,
-			stakeAmount:    units.MegaAvax,
-			existingAmount: 400 * units.MegaAvax,
-			expectedReward: 219298179771,
-		},
-		// (720M - 400M) * (2M / 400M) * (10% + 2% * MinimumStakingDuration / MaximumStakingDuration) * MinimumStakingDuration / MaximumStakingDuration
-		{
-			duration:       defaultMinStakingDuration,
-			stakeAmount:    2 * units.MegaAvax,
-			existingAmount: 400 * units.MegaAvax,
-			expectedReward: 438596359542,
-		},
-		// (720M - 720M) * (1M / 720M) * (10% + 2% * MinimumStakingDuration / MaximumStakingDuration) * MinimumStakingDuration / MaximumStakingDuration
-		{
-			duration:       defaultMinStakingDuration,
-			stakeAmount:    units.MegaAvax,
-			existingAmount: SupplyCap,
-			expectedReward: 0,
-		},
+func TestRewardYearTokenomic(t *testing.T) {
+	Duration := 31556952 //365 * 24 * time.Hour
+	Stake := units.MegaAvax
+	REWARD_POOL := 500 * units.MegaAvax
+	var TotalStake uint64 = 0
+	reward := rewardEZC(
+		uint(Duration),
+		fromEZC(float64(Stake)),
+		fromEZC(float64(TotalStake)),
+		fromEZC(float64(REWARD_POOL)),
+	)
+
+	REWARD_POOL = REWARD_POOL - reward
+	TotalStake += Stake
+
+	if reward > units.MegaAvax / 2 {
+		t.Fatalf("it seem first reward for first stake wrong formulation")
 	}
-	for _, test := range tests {
-		name := fmt.Sprintf("reward(%s,%d,%d)==%d",
-			test.duration,
-			test.stakeAmount,
-			test.existingAmount,
-			test.expectedReward,
+
+	// Total Stake	1	2	4	8	16	20	64	128	256	450	512	1024 ( mega avax )
+	// Percent Gain	50% 46% 42%	38%	34%	30%	26%	22%	18%	14%	10%	6%
+	// reset total and reward pool
+	// how total max supply is 1000, mean 500 for genesis in wallet
+	var stakeAmounts [256]uint64;
+	percentReward := [12]float64{50, 46, 42, 38, 34, 30, 26, 22, 18, 14, 10, 6}
+	TotalStake = 0
+	REWARD_POOL = 500 * units.MegaAvax
+	power := 0
+	for i := 0; i < 256; i++ {
+		reward := rewardEZC(
+			uint(Duration),
+			fromEZC(float64(Stake)),
+			fromEZC(float64(TotalStake)),
+			fromEZC(float64(REWARD_POOL)),
 		)
-		t.Run(name, func(t *testing.T) {
-			r := reward(
-				test.duration,
-				test.stakeAmount,
-				test.existingAmount,
-				defaultMaxStakingDuration,
-			)
-			if r != test.expectedReward {
-				t.Fatalf("expected %d; got %d", test.expectedReward, r)
-			}
-		})
+		stakeAmounts[i] = reward
+		REWARD_POOL -= reward
+		TotalStake += Stake
 	}
+
+
+	for i, _ := range stakeAmounts {
+		if isPowerTwo(i) {
+			percent := float64(stakeAmounts[i-1]) / float64(Stake) * 100
+			if percentReward[power] != percent {
+				t.Fatalf("reward caculate is wrong")
+			}
+			power += 1
+		}
+	}
+
+
 }
